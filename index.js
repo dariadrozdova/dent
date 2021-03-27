ee730647283ef407be4241201780c4c91eda8460
Daria Drozdova <dariadrozdova10@gmail.com>
2021-03-27 19:02:03 +0300

    feat: implement game

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const { Bot, Keyboard, KeyboardColor } = require('node-vk-bot');
 const util = require('util');
+const steps = require('./steps');
 
 const bot = new Bot({
-    token: 'твой_токен',
-    group_id: 777
+    token: 'b909f846c0f6cb4ef7437bddd7e829a544d1ed9a6359a01e3390a3926d744a73d718460a4287d02aadb92',
+    group_id: 203583457
 }).start();
 
 console.log('Bot started!');
 
 bot.get(/./i, (message, exec, reply) => {
-    
-    reply('Это всё что я могу делать - отвечать вот этим текстом!').catch(e => console.error(e));
+
+    let info = message.payload && steps[JSON.parse(message.payload)] || steps[''];
+    let keyboard = new Keyboard(true);
+    for (let i = 0; i < info.buttons.length; i++) {
+        if (i) keyboard.addRow()
+
+        const button = info.buttons[i];
+
+        keyboard.addButton(button.msg, KeyboardColor.PRIMARY, JSON.stringify(button.next));
+    }
+
+    reply(info.question, {keyboard}).catch(e => console.error(e));
 })
 
 bot.on('poll-error', error => {
